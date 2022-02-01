81848c9b16bd93b3aa85faffc1a04b8e38ac59a8
Olga <selikolya@mail.com>
2022-02-01 10:36:13 +0300

    update form

--- src/components/AddContact/validation.js
+++ src/components/AddContact/validation.js
@@ -2,8 +2,8 @@ const REGUIRED_FIELD = 'Обязательно для заполнения';
 export const nameValidation = {
     required: REGUIRED_FIELD,
     validate: (value) => {
-        if (value.match(/[a-zA-Z]/)){
-            return 'Имя может содержать только русские буквы'
+        if (value.match(/[0-9]/)){
+            return 'Имя не может содержать цифры'
         }
         return true;
     } 
@@ -12,11 +12,11 @@ export const nameValidation = {
 export const phoneValidation = {
     required: REGUIRED_FIELD,
     validate: (value) => {
-        if (value.match(/[]/)){
-            return 'Пароль не может содержать русские буквы'
+        if (value.match(/[а-яА-Я]/)){
+            return 'Телефон не может содержать русские буквы'
         }
-        if (value.length > 10) {
-            return 'Пароль не может содержать больше 10 символов'
+        if (value.length > 12) {
+            return 'Телефон не может содержать больше 12 символов'
         }
         return true;
     } 
